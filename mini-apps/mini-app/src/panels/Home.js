8258409ccdb37dff1e757304e6e6d6f1b70e4b48
Антон Зеунов <lalow.main@gmail.com>
2022-07-10 21:14:51 +0300

    edit endoint

--- mini-apps/mini-app/src/panels/Home.js
+++ mini-apps/mini-app/src/panels/Home.js
@@ -17,7 +17,7 @@ const Home = ({ id, go, fetchedUser }) => {
 	}
 
 	const getPhoto = () => {
-		axios.get(`https://ngrok.src.arenacmd.com//images`)
+		axios.get(`https://ngrok.src.arenacmd.com/images`)
 			.then(el => el.data)
 			.then(el => setDataImage(el))
 		console.log(dataImage)
@@ -43,7 +43,7 @@ const Home = ({ id, go, fetchedUser }) => {
 		// Fill with gradient
 		ctx.fillStyle = gradient;
 		ctx.fillText(textInput?.current?.value, 10, 70);
-		axios.post(`https://ngrok.src.arenacmd.com//images`, {
+		axios.post(`https://ngrok.src.arenacmd.com/images`, {
 			user: fetchedUser,
 			img:  ctx.canvas.toDataURL()
 		})
