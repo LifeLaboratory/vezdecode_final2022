4f6b7ec8970dd8ab326b8e5b6fb32e342f7a56d5
Boris Bochkarev <Boris-Bochkaryov@yandex.ru>
2022-07-10 10:09:22 +0700

    Add work with speech

--- app/src/main/java/ru/lifelaboratory/MainActivity.java
+++ app/src/main/java/ru/lifelaboratory/MainActivity.java
@@ -1,26 +1,41 @@
 package ru.lifelaboratory;
 
+import android.Manifest;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashSet;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    SpeechRecognizer speechRecognizer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,8 +84,82 @@ public class MainActivity extends AppCompatActivity {
 
         AlarmAdapter adapter = new AlarmAdapter(this, R.layout.alarm_item, new ArrayList<>(sharedPreferences.getStringSet("time", new HashSet<>())));
         productList.setAdapter(adapter);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 42);
+            }
+        }
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+
+        findViewById(R.id.btn_speech).setOnTouchListener((view, motionEvent) -> {
+            if (motionEvent.getAction() == MotionEvent.ACTION_UP){
+                speechRecognizer.stopListening();
+            }
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH);
+                speechRecognizer.startListening(speechRecognizerIntent);
+            }
+            return false;
+        });
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {}
+
+            @Override
+            public void onBeginningOfSpeech() {}
+
+            @Override
+            public void onRmsChanged(float v) {}
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {}
+
+            @Override
+            public void onEndOfSpeech() {}
+
+            @Override
+            public void onError(int i) {}
+
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onResults(Bundle bundle) {
+                ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                data.forEach(element -> {
+                    if (element.startsWith("Поставь будильник на")) {
+                        String time = element.replace("Поставь будильник на ", "");
+                        HashSet<String> setWithTimes = (HashSet<String>) sharedPreferences.getStringSet("time", new HashSet<>());
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        setWithTimes.add(time);
+                        editor.putStringSet("time", setWithTimes).apply();
+                        Toast.makeText(getApplicationContext(), "Будильник активирован на " + time, Toast.LENGTH_LONG).show();
+
+                        AlarmAdapter adapter = new AlarmAdapter(getApplicationContext(), R.layout.alarm_item, new ArrayList<>(sharedPreferences.getStringSet("time", new HashSet<>())));
+                        productList.setAdapter(adapter);
+                    }
+                });
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {}
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {}
+        });
     }
 
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 42 && grantResults.length > 0 ){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT).show();
+        }
+    }
 
 }
